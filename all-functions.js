622830f8d37d3d9a12be8af5b590d5e072f9778c
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-02-07 23:47:59 +0600

    problem 5 solved and assignment submitted

--- all-functions.js
+++ all-functions.js
@@ -2,7 +2,7 @@
 function mindGame(num){
     // checking if num is positive
     if(num < 0){
-        return  "Please Enter a Positive Number";
+        return  "Please Enter a POSITIVE Number";
     }
     // checkng if num is string
     if(typeof(num) === "string"){
@@ -16,9 +16,11 @@ function mindGame(num){
     return result;
 }
 
+
+
 /* Problem - 2: evenOdd() is a function checking if a string has even number of letters or not */
 function evenOdd(text){
-    if(typeof(text) === "number"){
+    if(typeof(text) === "number" || typeof(text) === "object"){
         return "Please Enter a String";
     }
     // storing the length of string
@@ -33,6 +35,8 @@ function evenOdd(text){
     }
 
 }
+
+
 
 /*Problem -3: isLGSeven() is a function checks the diiference of a number & 7, if difference is greater than 7 return the double of input otherwise return the difference */
 function isLGSeven(num){
@@ -52,6 +56,8 @@ function isLGSeven(num){
 
 }
 
+
+
 /* Problem -4: findingBadData() is a function checks how many good data (positive numbers) and bad data (negeative nubers) exists in an array */
 function findingBadData(ar){
     if(Array.isArray(ar) == false ){
@@ -66,4 +72,29 @@ function findingBadData(ar){
         }
     }
     return badData;
+}
+
+
+
+/* Problem - 5: gemsToDiamond()-> is a function that takes three parameters as input and return diamond number */
+function gemsToDiamond(input1, input2, input3){
+    if(arguments.length !== 3){
+        return "Please Enter Three Numbers"
+       }
+    else if(typeof(input1) !== "number" || typeof(input2) !== "number" || typeof(input3) !== "number") {
+        return "Please Enter a Valid Input"
+    }   
+     
+
+   let num1 = input1 *21;
+   let num2 = input2 *32;
+   let num3 = input3 *43;
+   let total = num1 + num2 + num3;
+   
+   if(total >= 2000){
+    return total-2000;
+   }
+
+   else return total;
+
 }
